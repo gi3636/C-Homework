9bdbe255d11b7c1b67c0f70fd7fdbec61c1ff3d6
fengg <fenggi123@gmail.com>
2020-02-19 23:06:38 +0800

    Calculator has finished

--- CSharpHomework/CSharpHomework/Program.cs
+++ CSharpHomework/CSharpHomework/Program.cs
@@ -10,7 +10,30 @@ namespace CSharpHomework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HEllo");
+            int number1;
+            int number2;
+            Console.WriteLine("Please input the number.");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input the second number.");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type the symbol of the arithmetic you want.");
+            String symbol = Console.ReadLine();
+            switch(symbol)
+            {
+                case "+":
+                    Console.WriteLine(number1+"+"+number2+"="+(number1+number2));
+                    break;
+                case "-":
+                    Console.WriteLine(number1 + "-" + number2 + "=" + (number1 - number2));
+                    break;
+                case "*":
+                    Console.WriteLine(number1 + "*" + number2 + "=" + (number1 * number2));
+                    break;
+                case "/":
+                    Console.WriteLine(number1 + "/" + number2 + "=" + (number1 / number2));
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
