50782ed3e5548412b4b0d1d7def3196b9f40a8f6
fengg <fenggi123@gmail.com>
2020-02-25 06:41:45 +0800

    Question1 had finished

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        public static object Mart { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.Write("请输入要分解的数：");
+            int n = int.Parse(Console.ReadLine());
+            Analyze(n);
+            Console.ReadKey();
+        }
+
+        private static void Analyze(int n)
+        {
+            Console.Write(n + "的因子有 ");
+            while (n % 2 == 0)
+            {
+                n = n / 2;
+                Console.Write("2 ");
+            }
+            while (n % 3 == 0)
+            {
+                n = n / 3;
+                Console.Write("3 ");
+            }
+            while(n%5==0)
+            {
+                n = n / 5;
+                Console.Write("5 ");
+            }
+            for(int i = 5;i <=n*3;i+=6)
+            {
+                while(n%i==0)
+                {
+                    n = n / i;
+                    Console.Write(i + " ");
+                }
+                while (n % (i + 2) == 0)
+                {
+                    n = n / (i + 2);
+                    Console.Write((i + 2) + " ");
+                }
+            }
+        }
+    }
+}
