7ba27b860011dae871cbcf3404eefcedee160cc5
gi3636 <fenggi123@gmail.com>
2020-03-02 08:05:19 +0800

    Computer had formatted ,it was a test

---
+++ Project3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");//电脑格式化了
+        }
+    }
+}
