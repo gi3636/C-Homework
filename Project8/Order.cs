0a0a9ea4f465080ae957e15e023ae427afcd7066
gi3636 <fenggi123@gmail.com>
2020-03-29 23:24:50 +0800

    I correct something and finished but the test still cannot fix it

---
+++ Project8/Order.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Project8
+{
+   public class Order
+    {
+        [XmlAttribute]
+        public String OrderID { get; set; }
+
+        [XmlAttribute]
+        public String CustomerID { get; set; }
+
+        [XmlAttribute]
+        public String ProductID { get; set; }
+
+        [XmlAttribute]
+        public  DateTime RequiredDate { get; set; }
+
+        [XmlAttribute]
+        public double SumPrice { get; set; }
+
+        [XmlAttribute]
+        public double Quantity { get; set; }
+
+        public Order()
+        {
+
+        }
+        public Order(string orderID, string customerID, string productID, DateTime requiredDate, double sumPrice, double quantity)
+        {
+            OrderID = orderID;
+            CustomerID = customerID;
+            ProductID = productID;
+            RequiredDate = requiredDate;
+            SumPrice = sumPrice;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return "OrderID:" + OrderID + " CustomerID:" + CustomerID + " ProductID:" + ProductID + " RequiredDate:" + RequiredDate +" Quantity:"+Quantity+ " SumPrice:" + SumPrice; 
+        }
+
+        public override bool Equals(object obj)
+        {
+            Order order1 = obj as Order;
+            return (order1 != null && order1.OrderID == OrderID &&  order1.Quantity == Quantity && order1.RequiredDate == RequiredDate &&order1.ProductID==ProductID &&order1.SumPrice==SumPrice);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -2027619230;
+            hashCode = hashCode * -1521134295 + OrderID.GetHashCode();
+            return hashCode;
+        }
+
+        
+    }
+
+}
---
+++ Project8/OrderItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project8
+{
+   public  class OrderItem
+    {
+        public String OrderID { get; set; }
+        public String ProductID { get; set; }
+        public double ProductPrice { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+        public override bool Equals(object obj)
+        {
+            OrderItem order1 = obj as OrderItem;
+            return (order1 != null && order1.OrderID == OrderID && order1.ProductID == ProductID && order1.ProductPrice == ProductPrice);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -2027619230;
+            hashCode = hashCode * -1521134295 + OrderID.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return "OrderID:" + OrderID + " ProductID:" + ProductID + " ProductPrice:" + ProductPrice;
+        }
+    }
+}
